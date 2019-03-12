ef5017edd2337e36dc117be4ba241d61d7b3ba0b
Chris Lewis <13chlewis@gmail.com>
2019-03-12 14:42:51 -0600

    Created single finders functionality and testing for Merchants

---
+++ spec/requests/api/v1/merchant_search_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  it "can search for a merchant with a query parameter" do
+    date = '2019-03-12 19:31:18 UTC'
+    assertion = '2019-03-12T19:31:18.000Z'
+    merchant = create(:merchant, created_at: date, updated_at: date)
+    id = merchant.id
+    name = merchant.name
+    create_list(:merchant, 5)
+
+    get "/api/v1/merchants/find?id=#{id}"
+    merchant = JSON.parse(response.body).symbolize_keys
+
+    expect(response).to be_successful
+    expect(merchant[:id]).to eq(id)
+
+    get "/api/v1/merchants/find?name=#{name}"
+    merchant = JSON.parse(response.body).symbolize_keys
+
+    expect(response).to be_successful
+    expect(merchant[:name]).to eq(name)
+
+    get "/api/v1/merchants/find?created_at=#{date}"
+    merchant = JSON.parse(response.body).symbolize_keys
+
+    expect(response).to be_successful
+    expect(merchant[:created_at]).to eq(assertion)
+
+    get "/api/v1/merchants/find?updated_at=#{date}"
+    merchant = JSON.parse(response.body).symbolize_keys
+
+    expect(response).to be_successful
+    expect(merchant[:updated_at]).to eq(assertion)
+  end
+end
