1a88a6602b3237ead0e8cd974c51f6c3a81ed961
Chris Lewis <13chlewis@gmail.com>
2019-03-12 13:58:09 -0600

    Added test and functionality for merchant show endpoint

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Merchants API" do
-  it "can return an index of merchants" do
+  it "can return all merchant records" do
     create_list(:merchant, 3)
 
     get "/api/v1/merchants.json"
@@ -11,5 +11,18 @@ describe "Merchants API" do
     merchants = JSON.parse(response.body)
 
     expect(merchants.count).to eq(3)
+  end
+
+  it "can return a merchant record by id" do
+    id = create(:merchant).id
+    create_list(:merchant, 3)
+
+    get "/api/v1/merchants/#{id}.json"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body).symbolize_keys
+
+    expect(merchant[:id]).to eq(id)
   end
 end
