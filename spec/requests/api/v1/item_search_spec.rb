95735a8b0822d6703d89cf12065a8e921378ed71
Chris Lewis <13chlewis@gmail.com>
2019-03-14 21:38:57 -0600

    Created multi-finder endpoint for Items

--- spec/requests/api/v1/item_search_spec.rb
+++ spec/requests/api/v1/item_search_spec.rb
@@ -56,4 +56,57 @@ describe "Items API" do
     expect(response).to be_successful
     expect(item["data"]["attributes"]["updated_at"]).to eq(@assertion)
   end
+
+  it "can search for all items matching a query parameter" do
+    create(:item, name: 'name', description: 'description', unit_price: 2000, merchant_id: @merchant_id, created_at: @date, updated_at: @date)
+
+    get "/api/v1/items/find_all?id=#{@id}"
+    items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(items.count).to eq(1)
+    expect(items["data"].first["id"]).to eq(@id.to_s)
+
+    get "/api/v1/items/find_all?name=#{@name}"
+    items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(items["data"].count).to eq(2)
+    expect(items["data"].first["attributes"]["name"]).to eq(@name)
+
+    get "/api/v1/items/find_all?description=#{@description}"
+    items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(items["data"].count).to eq(2)
+    expect(items["data"].first["attributes"]["description"]).to eq(@description)
+
+    get "/api/v1/items/find_all?unit_price=#{@unit_price}"
+    items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(items["data"].count).to eq(2)
+    expect(items["data"].first["attributes"]["unit_price"]).to eq(@unit_price)
+
+    get "/api/v1/items/find_all?merchant_id=#{@merchant_id}"
+    items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(items["data"].count).to eq(2)
+    expect(items["data"].first["attributes"]["merchant_id"]).to eq(@merchant_id)
+
+    get "/api/v1/items/find_all?created_at=#{@date}"
+    items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(items["data"].count).to eq(2)
+    expect(items["data"].first["attributes"]["created_at"]).to eq(@assertion)
+
+    get "/api/v1/items/find_all?updated_at=#{@date}"
+    items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(items["data"].count).to eq(2)
+    expect(items["data"].first["attributes"]["updated_at"]).to eq(@assertion)
+  end
 end
