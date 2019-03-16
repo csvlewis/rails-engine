f1cd626c48cb961aa1053f3dd3f16e2cfd0460db
Chris Lewis <13chlewis@gmail.com>
2019-03-16 11:44:49 -0600

    Added simplecov gem for test coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
+
+require 'simplecov'
+SimpleCov.start
+
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
